071b8db126314f76b9d12c033fe4804e7991ee39
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2026-08-20 19:51:48 +1000

    Update .gitignore to rename environment file from .env.deploy to .deploy.env for clarity

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv");
+
+dotenv.config({ path: ".deploy.env" });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } =
+  process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "mesto-api",
+      script: "./dist/app.js",
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      "pre-deploy-local": `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      "post-deploy":
+        "cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+    },
+  },
+};
